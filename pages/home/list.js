17742bc3f3ddfbac3774eff28aa597fd968d0088
hby <473249753@qq.com>
2018-10-21 22:49:15 +0800

    创建数据库，构造查询器

---
+++ pages/home/list.js
@@ -0,0 +1,82 @@
+// pages/home/list.js
+import Storage from "../../lib/storage.js";
+const db= new Storage("test");
+db.add([
+  {a:1,
+  id:2
+  },
+  {a:24446,
+  id:2
+  }
+]).save()
+//当我调用where的时候并不是真正的查询，而是构建查询语句
+
+let str =(db.where("id", 2).find());
+console.log(str);
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+   const val =wx.getStorageSync("test");
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
